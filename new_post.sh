c1329ed44df735aa6f5ff0a198eb4ce5c86e3da8
khanhtc1202 <khanhtc1202@gmail.com>
2017-12-24 14:53:20 +0000

    create markdown file of new post

---
+++ new_post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# parse params
+while [[ -z "$1" ]]
+do
+    echo "Missing post's name..."
+    exit 1
+done
+
+# define var
+TEMP="---\ntitle:\ndate: $(date +"%Y-%m-%d %T")\ntags:\nthumbnail:\n---\n"
+SRC_DIR="./source/_posts/"
+MD_PATH=$SRC_DIR$1".md"
+
+echo -e $TEMP > $MD_PATH
